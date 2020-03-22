5e72e27c72e5318755672b4387c7c759f81cc8cf
georgevoliveira <52459984+georgevoliveira@users.noreply.github.com>
2020-03-22 12:29:52 +0000

    Update Configuration.h

--- TFT/src/User/Configuration.h
+++ TFT/src/User/Configuration.h
@@ -55,8 +55,8 @@
 
 // Marlin Mode Background & Font Color Options
 // Current color options from lcd.h: BLACK, BLUE, BROWN, BRRED, CYAN, GBLUE, GRAY, GREEN, MAGENTA, RED, WHITE, YELLOW
-#define ST7920_BKCOLOR BLACK
-#define ST7920_FNCOLOR YELLOW
+#define ST7920_BKCOLOR WHITE
+#define ST7920_FNCOLOR BLACK
 
 /**
  *  Text displayed at the top of the LCD in Marlin Mode.
@@ -67,7 +67,7 @@
  * Run Marlin Mode fullscreen.
  * Not recommended for TFT24.
  */
-//#define ST7920_FULLSCREEN
+#define ST7920_FULLSCREEN
 
 /**
  * CLEAN MODE SWITCHING SUPPORT
@@ -143,9 +143,9 @@
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS 235
-#define Y_MAX_POS 235
-#define Z_MAX_POS 250
+#define X_MAX_POS 350
+#define Y_MAX_POS 350
+#define Z_MAX_POS 400
 
 // Specify a pause position as { X, Y, Z_raise }
 #define NOZZLE_PAUSE_RETRACT_LENGTH 15   // (mm)
